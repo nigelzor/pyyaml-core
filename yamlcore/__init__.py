fd9989e673e1e0f7b72029717f39dc3586927ee8
Neil Gentleman <ngentleman@gmail.com>
2025-03-26 15:27:23 -0700

    don't throw if libyaml isn't available

--- yamlcore/__init__.py
+++ yamlcore/__init__.py
@@ -4,15 +4,6 @@ from yamlcore.dumper import CommonRepresenter
 
 
 class CommonLoader(yaml.BaseLoader, CommonConstructor, CommonResolver):
-
-    @classmethod
-    def init_tagset(cls, tagset):
-        cls.init_constructors(tagset)
-        cls.init_resolvers(tagset)
-
-
-class CCommonLoader(yaml.CBaseLoader, CommonConstructor, CommonResolver):
-
     @classmethod
     def init_tagset(cls, tagset):
         cls.init_constructors(tagset)
@@ -23,20 +14,7 @@ class CoreLoader(CommonLoader):
     pass
 
 
-class CCoreLoader(CCommonLoader):
-    pass
-
-
 class CommonDumper(yaml.BaseDumper, CommonRepresenter, CommonResolver):
-
-    @classmethod
-    def init_tags(cls, tagset):
-        cls.init_representers(tagset)
-        cls.init_resolvers(tagset)
-
-
-class CCommonDumper(yaml.CBaseDumper, CommonRepresenter, CommonResolver):
-
     @classmethod
     def init_tags(cls, tagset):
         cls.init_representers(tagset)
@@ -47,11 +25,33 @@ class CoreDumper(CommonDumper):
     pass
 
 
-class CCoreDumper(CCommonDumper):
-    pass
-
-
 CoreLoader.init_tagset('core')
-CCoreLoader.init_tagset('core')
 CoreDumper.init_tags('core')
-CCoreDumper.init_tags('core')
+
+
+if yaml.__with_libyaml__:
+
+    class CCommonLoader(yaml.CBaseLoader, CommonConstructor, CommonResolver):
+        @classmethod
+        def init_tagset(cls, tagset):
+            cls.init_constructors(tagset)
+            cls.init_resolvers(tagset)
+
+
+    class CCoreLoader(CCommonLoader):
+        pass
+
+
+    class CCommonDumper(yaml.CBaseDumper, CommonRepresenter, CommonResolver):
+        @classmethod
+        def init_tags(cls, tagset):
+            cls.init_representers(tagset)
+            cls.init_resolvers(tagset)
+
+
+    class CCoreDumper(CCommonDumper):
+        pass
+
+
+    CCoreLoader.init_tagset('core')
+    CCoreDumper.init_tags('core')
